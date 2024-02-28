fe9fa6327dba69e8cf00d8d5bf4f6fd3add5a3d0
Niggling <272744476@qq.com>
2024-02-28 15:05:30 +0800

    TTS服务器

--- run.py
+++ run.py
@@ -20,4 +20,4 @@ def synthesize():
         return jsonify({"error": "No text provided."}), 400
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=8080)
+    app.run(host='0.0.0.0', port=8081)
