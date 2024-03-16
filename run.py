31451a325b3e4fb643165dea96dfe9a9a62d2ba7
Niggling <272744476@qq.com>
2024-03-16 14:17:26 +0800

    TTS服务器

--- run.py
+++ run.py
@@ -1,23 +1,47 @@
+import logging
+import os
 import torch
 from TTS.api import TTS
 from flask import Flask, request, jsonify
 
 app = Flask(__name__)
 
-# Get device
+# 配置日志
+logging.basicConfig(level=logging.DEBUG)  # 设置日志级别为DEBUG，记录所有级别的日志消息
+
+# 获取设备
 device = "cuda" if torch.cuda.is_available() else "gpu"
 tts = TTS("tts_models/zh-CN/baker/tacotron2-DDC-GST")
 
-@app.route('/synthesize', methods=['POST'])
+# 设置音频文件保存路径
+audio_directory = "audio"
+
+@app.route('/synthesize', methods=['GET'])
 def synthesize():
-    data = request.json
-    text = data.get('text', '')
+    logging.info("Audio generation request received.")
+    text = request.args.get('text', '')
     if text:
-        file_path = "audio/test6.wav"  # 修改为你想要保存音频的路径
+        logging.info("Audio generation started.")
+        # 生成音频文件的文件名
+        file_name = "test10.wav"
+        file_path = os.path.join(audio_directory, file_name)  # 构建完整的文件路径
         tts.tts_to_file(text=text, file_path=file_path)
-        return jsonify({"message": "Audio generated successfully."}), 200
+        # 记录日志消息
+        logging.info("Audio generated successfully.")
+        # 返回带有CORS头信息的响应，包括音频文件的URL
+        audio_url = request.host_url + file_path.replace("\\", "/")  # 构建音频文件的URL，并替换反斜杠为正斜杠
+        response = jsonify({"message": "Audio generated successfully.", "audioUrl": audio_url})
+        response.headers.add('Access-Control-Allow-Origin', 'http://localhost:8080')
+        return response, 200
     else:
-        return jsonify({"error": "No text provided."}), 400
+        # 记录日志消息
+        logging.error("No text provided.")
+        response = jsonify({"error": "No text provided."})
+        response.headers.add('Access-Control-Allow-Origin', 'http://localhost:8080')
+        return response, 400
 
 if __name__ == '__main__':
+    # 确保音频文件保存目录存在
+    if not os.path.exists(audio_directory):
+        os.makedirs(audio_directory)
     app.run(host='0.0.0.0', port=8081)
